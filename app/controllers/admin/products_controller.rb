ec18f94234a620f506967b5596f7bcb4ebde3004
jioherry <jioherry@gmail.com>
2017-05-28 21:20:23 +0800

    fix admin CRUD bug

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,53 +1,47 @@
 class Admin::ProductsController < ApplicationController
 
-	before_action :authenticate_user!
-	before_action :set_product, only:[:edit, :update, :destroy]
+  before_action :authenticate_user!
+  before_action :set_product, only: [:edit, :update, :destroy]
 
-	def index
-		@products = Product.page(params[:page]).page(50)
-	end
+  def index
+    @products = Product.page(params[:page]).per(50)
+  end
 
-	def new
-		@product = Product.new
-	end
+  def new
+    @product = Product.new
+  end
 
-	def create
-		@product = Product.new(product_params)
+  def create
+    @product = Product.new(product_params)
 
-		if @product.save
-			redirect_to admin_products_path, notice: 'product was successfully created'
-		else
-			render new_admin_product_path
-		end
-	end
+    if @product.save
+      redirect_to admin_products_path, notice: 'product was successfully created'
+    else
+      render new_admin_product_path
+    end
+  end
 
-	def update
-		if @product.update(product_params)
-			redirect_to admin_products_path, notice: 'update successfully'
-		else
-			render action: :edit
-		end 
-	end
+  def update
+    if @product.update(product_params)
+      redirect_to admin_products_path, notice: 'update successfully'
+    else
+      render action: :edit
+    end
+  end
 
-	def destroy 
-		@product.destroy
-		redirect_to admin_products_path, alert: 'product deleted'
-	end
-		
-	end
+  def destroy
+    @product.destroy
+    redirect_to admin_products_path, alert: 'product deleted.'
+  end
 
-	private
+  private
 
-		def set_product
-			@product = Product.find(params[:id])
-		end
+  def set_product
+    @product = Product.find(params[:id])
+  end
 
-		def product_params
-			params.require(:product).permit(:name,
-																			:image_url,
-																			:price,
-																			:description,
-																			:in_stock_qty)
-		end
+  def product_params
+    params.require(:product).permit(:name, :price, :image_url, :description, :in_stock_qty)
+  end
 
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,11 @@
 class ProductsController < ApplicationController
 
-	def index
-		@products = Product.page(params[:page]).per(50)
-	end
+  def index
+    @products = Product.page(params[:page]).per(50)
+  end
 
-	def show
-		@product = Product.find(params[:id])
-	end
+  def show
+    @product = Product.find(params[:id])
+  end
 
 end
