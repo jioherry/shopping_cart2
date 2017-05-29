fc7ef412e867ef4e081474865a92dad4a2e5c9eb
jioherry <jioherry@gmail.com>
2017-05-29 19:35:52 +0800

    user can check order(move cart items to order items)

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,32 @@
+class OrdersController < ApplicationController
+
+  before_action :authenticate_user! # Visitor 在這裡第一次登入，成為 Customer
+
+  def new
+    @order = current_user.orders.build(	:email => current_user.email)
+  end
+
+  def show
+    @order = Order.find(params[:id])
+  end
+
+  def create
+    @order = current_user.orders.build(order_params)
+    @order.add_order_items(current_cart)
+    @order.amount = current_cart.total
+
+    if @order.save
+      current_cart.destroy
+      redirect_to order_path(@order), notice: '已結帳！'
+    else
+      render :new
+    end
+  end
+
+	protected
+
+		def order_params
+			params.require(:order).permit(:name, :phone, :address, :email, :payment_method)
+		end
+
+end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,19 @@
 class Order < ApplicationRecord
 
+	validates_presence_of :name, :email, :address, :phone, :payment_method
+
   belongs_to :user
   has_many :order_items, :dependent => :destroy
   has_many :products, through: :order_items
 
   enum payment_method: [ :credit, :atm]
+
+  def add_order_items(cart)
+  	cart.cart_items.each do |item|
+  		self.order_items.build(	:product_id => item.product_id,
+  														:quantity => item.quantity,
+  														:price => item.product.price)
+  	end
+  end
+
 end
